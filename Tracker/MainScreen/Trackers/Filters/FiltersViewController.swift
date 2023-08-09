c16a4881a70ee6d685d8c2708b1beb3a76da996e
fanem <nai90@ya.ru>
2023-08-09 14:03:18 +0200

    Moved FilterType to a separate file

--- Tracker/MainScreen/Trackers/Filters/FiltersViewController.swift
+++ Tracker/MainScreen/Trackers/Filters/FiltersViewController.swift
@@ -80,26 +80,3 @@ extension FiltersViewController: UITableViewDelegate {
         }
     }
 }
-
-// MARK: - FilterType
-enum FilterType: String, Codable {
-    case all
-    case today
-    case completed
-    case inProgress
-    
-    var name: String {
-        var name: String
-        switch self {
-        case .all:
-            name = "All trackers"
-        case .today:
-            name = "Trackers for today"
-        case .completed:
-            name = "Сompleted"
-        case .inProgress:
-            name = "Not completed"
-        }
-        return name.localized()
-    }
-}
