3ab7a9584df72dd2adad4a0a237debd6feedde2e
fanem <nai90@ya.ru>
2023-08-09 13:03:11 +0200

    Padding has been added to TextField init. Offset was added for clearButton.

--- Tracker/Base/TextField.swift
+++ Tracker/Base/TextField.swift
@@ -7,10 +7,11 @@ import UIKit
 
 // MARK: - TextField
 final class TextField: UITextField {
-    let padding = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 40)
+    let padding: UIEdgeInsets
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
+    init(padding: UIEdgeInsets = UIEdgeInsets(top: 0, left: 16, bottom: 0, right: 40)) {
+        self.padding = padding
+        super.init(frame: .zero)
         backgroundColor = .trBackground
         layer.cornerRadius = GlobalConstants.cornerRadius
         clearButtonMode = .whileEditing
@@ -19,6 +20,11 @@ final class TextField: UITextField {
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func clearButtonRect(forBounds bounds: CGRect) -> CGRect {
+        let originalRect = super.clearButtonRect(forBounds: bounds)
+        return originalRect.offsetBy(dx: -10, dy: 0)
     }
 
     override func textRect(forBounds bounds: CGRect) -> CGRect {
